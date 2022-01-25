02e49707285a90ac76d1da56fbdc44a271bd40a8
ismailhssini <86742132+ismailhssini@users.noreply.github.com>
2022-01-25 02:47:27 +0100

    Add files via upload

---
+++ EX2.py
@@ -0,0 +1,33 @@
+import datetime 
+
+class Person:
+
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+        self.address = address
+        self.telephone = telephone
+        self.email = email
+    def _recalculate_age(self):
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+
+        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+            age -= 1
+
+        self._age = age
+        self._age_last_recalculated = today
+
+    def age(self):
+        if (datetime.date.today() > self._age_last_recalculated):
+            self._recalculate_age()
+        return self._age
+person = Person(
+    "Jane",
+    "Doe",
+    datetime.date(1992, 3, 12), # year, month, day
+    "No. 12 Short Street, Greenville",
+    "555 456 0987",
+    "jane.doe@example.com"
+)
